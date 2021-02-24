0252aeb8bc34d0e560337b58e7c847659118c5e8
DKKs <eye.q.21@gmail.com>
2021-02-25 03:15:24 +0700

    [Add] tailwind and navbar

---
+++ src/component/navbar.js
@@ -0,0 +1,11 @@
+function Navbar () {
+    return <>
+        <nav className="flex justify-between">
+            <div className="p-4">1</div>
+            <div>1</div>
+            <div>1</div>
+        </nav>
+    </>
+}
+
+export { Navbar }
